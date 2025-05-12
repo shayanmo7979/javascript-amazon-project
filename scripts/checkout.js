e34649f50a7d5a0f10ed6e5f2a303121076358f0
Shayan Moghaddam <shayanmo7979@gmail.com>
2025-05-12 11:46:26 -0700

    Converted loadProducts from XHR to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,18 +1,32 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 // import '../data/cart-oop.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js'
 
-new Promise((resolve) => {
-  loadProducts(() => {
-    resolve()
-  })
-}).then(() => {
+loadProductsFetch().then(() => {
   renderOrderSummary();
   renderPaymentSummary();
 })
+
+// new Promise((resolve) => {
+//   loadProductsFetch(() => {
+//     resolve()
+//   })
+// }).then(() => {
+//   renderOrderSummary();
+//   renderPaymentSummary();
+// })
+
+// new Promise((resolve) => {
+//   loadProducts(() => {
+//     resolve()
+//   })
+// }).then(() => {
+//   renderOrderSummary();
+//   renderPaymentSummary();
+// })
 
 // loadProducts(() => {
 //   renderOrderSummary();
